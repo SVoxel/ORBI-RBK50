fb363a3c2f10b5fab15f7cbaf40896aebf5423b7
voxel <voxel@andreev.msk.ru>
2022-04-05 19:17:24 +0300

    Fix PATH settings in 'compile.sh' script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 export TZ=UTC
+PATH=/home/voxel/local/bin:/home/voxel/go/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+/usr/bin/make tools/install V=s >& reptools
+make -j4 toolchain/install V=s >& reptoolchain
 make V=99 >& report
